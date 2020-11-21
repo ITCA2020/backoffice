7e98eb834a706422ebc3579512dbae475f6b81d6
e-slzr <eliiuslzr@gmail.com>
2020-11-21 02:52:19 -0600

    backoffice-riconchita

---
+++ rinconchita.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-11-2020 a las 06:21:28
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `rinconchita`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almuerzos`
+--
+
+CREATE TABLE `almuerzos` (
+  `id_alm` int(11) NOT NULL,
+  `alm_nom` varchar(100) DEFAULT NULL,
+  `alm_precio` float DEFAULT NULL,
+  `alm_desc` varchar(50) DEFAULT NULL,
+  `alm_img` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aperitivos`
+--
+
+CREATE TABLE `aperitivos` (
+  `id_ape` int(11) NOT NULL,
+  `ape_nom` varchar(100) DEFAULT NULL,
+  `ape_precio` float DEFAULT NULL,
+  `ape_desc` varchar(100) DEFAULT NULL,
+  `ape_img` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bebidas`
+--
+
+CREATE TABLE `bebidas` (
+  `id_beb` int(11) NOT NULL,
+  `beb_nom` varchar(100) DEFAULT NULL,
+  `beb_precio` float DEFAULT NULL,
+  `beb_desc` varchar(100) DEFAULT NULL,
+  `beb_img` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `desayunos`
+--
+
+CREATE TABLE `desayunos` (
+  `id_des` int(11) NOT NULL,
+  `des_nom` varchar(50) DEFAULT NULL,
+  `des_precio` float DEFAULT NULL,
+  `des_desc` varchar(50) DEFAULT NULL,
+  `des_img` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `info`
+--
+
+CREATE TABLE `info` (
+  `wsp` varchar(100) DEFAULT NULL,
+  `tel` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_pedido` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `user_dir` varchar(100) DEFAULT NULL,
+  `producto` varchar(50) DEFAULT NULL,
+  `cant` varchar(50) DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postres`
+--
+
+CREATE TABLE `postres` (
+  `id_pos` int(11) NOT NULL,
+  `pos_nom` varchar(100) DEFAULT NULL,
+  `pos_precio` float DEFAULT NULL,
+  `pos_desc` varchar(100) DEFAULT NULL,
+  `pos_img` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_user` int(11) NOT NULL,
+  `user_nom` varchar(50) DEFAULT NULL,
+  `user_pass` varchar(50) DEFAULT NULL,
+  `user_correo` varchar(50) DEFAULT NULL,
+  `user_dir` varchar(100) DEFAULT NULL,
+  `user_tel` varchar(50) DEFAULT NULL,
+  `user_img` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `almuerzos`
+--
+ALTER TABLE `almuerzos`
+  ADD PRIMARY KEY (`id_alm`);
+
+--
+-- Indices de la tabla `aperitivos`
+--
+ALTER TABLE `aperitivos`
+  ADD PRIMARY KEY (`id_ape`);
+
+--
+-- Indices de la tabla `bebidas`
+--
+ALTER TABLE `bebidas`
+  ADD PRIMARY KEY (`id_beb`);
+
+--
+-- Indices de la tabla `desayunos`
+--
+ALTER TABLE `desayunos`
+  ADD PRIMARY KEY (`id_des`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indices de la tabla `postres`
+--
+ALTER TABLE `postres`
+  ADD PRIMARY KEY (`id_pos`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `almuerzos`
+--
+ALTER TABLE `almuerzos`
+  MODIFY `id_alm` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `aperitivos`
+--
+ALTER TABLE `aperitivos`
+  MODIFY `id_ape` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `bebidas`
+--
+ALTER TABLE `bebidas`
+  MODIFY `id_beb` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `desayunos`
+--
+ALTER TABLE `desayunos`
+  MODIFY `id_des` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `postres`
+--
+ALTER TABLE `postres`
+  MODIFY `id_pos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuarios` (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
