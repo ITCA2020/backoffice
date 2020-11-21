87c5fab9e8bff806b7eb8e1426cc15ae0bf3603d
e-slzr <eliiuslzr@gmail.com>
2020-11-21 02:50:36 -0600

    backoffice-riconchita

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+    <title>Starter Template · Bootstrap</title>
+    
+    <!--Template based on URL below-->
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css">
+
+    <!-- Place your stylesheet here-->
+    <link href="/css/stylesheet.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">Riconchita | Administración</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menú</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <label class="dropdown-item" onclick="mostrarAlmuerzos()">Almuerzos</label>
+                    <label class="dropdown-item" onclick="mostrarAperitivos()">Aperitivos</label>
+                    <label class="dropdown-item" onclick="mostrarBebidas()">Bebidas</label>
+                    <label class="dropdown-item" onclick="mostrarDesayunos()">Desayunos</label>
+                    <label class="dropdown-item" onclick="mostrarPostres()">Postres</label>
+                </div>
+            </li>
+            <li class="nav-item">
+                <label class="nav-link" onclick="mostrarPedidos()">Pedidos</label>
+            </li>
+            <li class="nav-item">
+                <label class="nav-link" onclick="mostrarClientes()">Clientes</label>
+            </li>
+            <li class="nav-item">
+                <label class="nav-link" onclick="mostrarInfo()">Información</label>
+            </li>
+        </ul>
+        
+    </div>
+</nav>
+
+    <main role="main" class="container">
+ 
+       
+        <!--
+        *    *    *    *    *    *    *    *    *    *    *    *
+                        FORMULARIO PARA ALMUERZOS
+        *    *    *    *    *    *    *    *    *    *    *    *
+        -->
+       
+        <div class="text-center mt-5 pt-5 contenedor_principal" id="almuerzos">
+            <h1>Registrar Almuerzos</h1><hr>
+            
+            <div class="" id="msj_almuerzo">
+                
+            </div>
+            
+            <form action="">
+                <div class="form lead">
+                    <input type="text" placeholder="ID" class="form-control" readonly>
+                    <input type="text" placeholder="Nombre del almuerzo" class="form-control">
+                    <input type="number" placeholder="Precio" class="form-control">
+                    <input type="text" placeholder="Descripción" class="form-control">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
+                    </div><hr>
+                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    
+                </div>
+            </form>
+        </div>
+        
+        
+        
+        <!--
+        *    *    *    *    *    *    *    *    *    *    *    *
+                        FORMULARIO PARA DESAYUNOS
+        *    *    *    *    *    *    *    *    *    *    *    *
+        -->
+       
+        <div class="text-center mt-5 pt-5 contenedor_principal" id="desayunos">
+            <h1>Registrar Desayunos</h1><hr>
+            
+            <div class="" id="msj_desayuno"></div>
+            
+            <form action="">
+                <div class="form lead">
+                    <input type="text" placeholder="ID" class="form-control" readonly>
+                    <input type="text" placeholder="Nombre del desayuno" class="form-control">
+                    <input type="number" placeholder="Precio" class="form-control">
+                    <input type="text" placeholder="Descripción" class="form-control">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
+                    </div><hr>
+                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    
+                </div>
+            </form>
+        </div>
+        
+        
+        
+        <!--
+        *    *    *    *    *    *    *    *    *    *    *    *
+                       FORMULARIO PARA APERITIVOS
+        *    *    *    *    *    *    *    *    *    *    *    *
+        -->
+       
+        <div class="text-center mt-5 pt-5 contenedor_principal" id="aperitivos">
+            <h1>Registrar Aperitivos</h1><hr>
+            <div class="" id="msj_aperitivo"></div>
+            
+            <form action="">
+                <div class="form lead">
+                    <input type="text" placeholder="ID" class="form-control" readonly>
+                    <input type="text" placeholder="Nombre del aperitivo" class="form-control">
+                    <input type="number" placeholder="Precio" class="form-control">
+                    <input type="text" placeholder="Descripción" class="form-control">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
+                    </div><hr>
+                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    
+                </div>
+            </form>
+        </div>
+        
+        
+        
+        <!--
+        *    *    *    *    *    *    *    *    *    *    *    *
+                       FORMULARIO PARA POSTRES
+        *    *    *    *    *    *    *    *    *    *    *    *
+        -->
+       
+        <div class="text-center mt-5 pt-5 contenedor_principal" id="postres">
+            <h1>Registrar Postres</h1><hr>
+            <div class="" id="msj_postre"></div>
+            
+            <form action="">
+                <div class="form lead">
+                    <input type="text" placeholder="ID" class="form-control" readonly>
+                    <input type="text" placeholder="Nombre del postre" class="form-control">
+                    <input type="number" placeholder="Precio" class="form-control">
+                    <input type="text" placeholder="Descripción" class="form-control">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
+                    </div><hr>
+                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    
+                </div>
+            </form>
+        </div>
+        
+        
+        
+        <!--
+        *    *    *    *    *    *    *    *    *    *    *    *
+                       FORMULARIO PARA BEBIDAS
+        *    *    *    *    *    *    *    *    *    *    *    *
+        -->
+       
+        <div class="text-center mt-5 pt-5 contenedor_principal" id="bebidas">
+            <h1>Registrar Bebidas</h1><hr>
+            <div class="" id="msj_bebida"></div>
+            
+            <form action="">
+                <div class="form lead">
+                    <input type="text" placeholder="ID" class="form-control" readonly>
+                    <input type="text" placeholder="Nombre de bebida" class="form-control">
+                    <input type="number" placeholder="Precio" class="form-control">
+                    <input type="text" placeholder="Descripción" class="form-control">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
+                    </div><hr>
+                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    
+                </div>
+            </form>
+        </div>
+        
+        
+        
+        <!--
+        *    *    *    *    *    *    *    *    *    *    *    *
+                           REGISTRO DE PEDIDOS
+        *    *    *    *    *    *    *    *    *    *    *    *
+        -->
+       
+        <div class="text-center mt-5 pt-5 contenedor_principal" id="pedidos">
+            <h1>Registro de Pedidos</h1>
+            <div class="" id="msj_pedidos"></div><hr>
+            
+            <div class="form-inline">
+                <input class="form-control" type="search" placeholder="Buscar.." aria-label="Buscar">
+                <button class="btn btn-primary" type="buttom" id="btnBuscar" onclick="">Buscar</button>
+            </div>
+            
+            <table class="table table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th></th>
+                        <th>PEDIDO</th>
+                        <th>FECHA</th>
+                        <th>HORA</th>
+                        <th>CLIENTE</th>
+                        <th>MONTO</th>
+                        <th>ESTADO</th>
+                        <th>acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><input type="radio" name="pedido"></td>
+                        <td>00000</td>
+                        <td>28 de dic de 2020</td>
+                        <td>11:30 AM</td>
+                        <td>Eliu Salazar</td>
+                        <td>$ 25.00</td>
+                        <td>recibido</td>
+                        <td>
+                           <div class="btn-group">
+                                <button type="button" class="btn btn-success">Modificar</button>
+                                <button type="button" class="btn btn-danger">Eliminar</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+        
+        
+        <!--
+        *    *    *    *    *    *    *    *    *    *    *    *
+                           REGISTRO DE CLIENTES
+        *    *    *    *    *    *    *    *    *    *    *    *
+        -->
+       
+        <div class="text-center mt-5 pt-5 contenedor_principal" id="clientes">
+            <h1>Clientes Registrados</h1>
+            <div class="" id="msj_clientes"></div><hr>
+            
+            <div class="form-inline">
+                <input class="form-control" type="search" placeholder="Buscar.." aria-label="Buscar">
+                <button class="btn btn-primary" type="buttom" id="btnBuscar" onclick="">Buscar</button>
+            </div>
+            
+            <table class="table table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th></th>
+                        <th>id</th>
+                        <th>cliente</th>
+                        <th>dirección</th>
+                        <th>teléfono</th>
+                        <th>acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><input type="radio" name="cliente"></td>
+                        <td>00000</td>
+                        <td class="alinear_izq">Edenilson Salas</td>
+                        <td class="alinear_izq">Col. Santa Leonor, Pasaje 8 Casa 9, Santa Ana</td>
+                        <td>2456-4658</td>
+                        <td>
+                           <div class="btn-group">
+                                <button type="button" class="btn btn-success">Modificar</button>
+                                <button type="button" class="btn btn-danger">Eliminar</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+        
+        
+        <!--
+        *    *    *    *    *    *    *    *    *    *    *    *
+                       FORMULARIO PARA INFORMACION
+        *    *    *    *    *    *    *    *    *    *    *    *
+        -->
+       
+        <div class="text-center mt-5 pt-5 contenedor_principal" id="info">
+            <h1>Información de Contacto</h1><hr>
+            <div class="" id="msj_bebida"></div>
+            
+            <form action="">
+                <div class="form lead">
+                    <input type="text" placeholder="Número de Whatsapp" class="form-control">
+                    <input type="number" placeholder="Número de Teléfono" class="form-control">
+                    <hr>
+                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    
+                </div>
+            </form>
+        </div>
+
+    </main><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+    <script src="script.js"></script>
+
+
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,80 @@
+function mostrarAlmuerzos(){
+    document.getElementById("almuerzos").style.display='flex';
+    document.getElementById("desayunos").style.display='none';
+    document.getElementById("aperitivos").style.display='none';
+    document.getElementById("postres").style.display='none';
+    document.getElementById("bebidas").style.display='none';
+    document.getElementById("clientes").style.display='none';
+    document.getElementById("pedidos").style.display='none';
+    document.getElementById("info").style.display='none';
+}
+function mostrarDesayunos(){
+    document.getElementById("almuerzos").style.display='none';
+    document.getElementById("desayunos").style.display='flex';
+    document.getElementById("aperitivos").style.display='none';
+    document.getElementById("postres").style.display='none';
+    document.getElementById("bebidas").style.display='none';
+    document.getElementById("clientes").style.display='none';
+    document.getElementById("pedidos").style.display='none';
+    document.getElementById("info").style.display='none';
+}
+function mostrarAperitivos(){
+    document.getElementById("almuerzos").style.display='none';
+    document.getElementById("desayunos").style.display='none';
+    document.getElementById("aperitivos").style.display='flex';
+    document.getElementById("postres").style.display='none';
+    document.getElementById("bebidas").style.display='none';
+    document.getElementById("clientes").style.display='none';
+    document.getElementById("pedidos").style.display='none';
+    document.getElementById("info").style.display='none';
+}
+function mostrarPostres(){
+    document.getElementById("almuerzos").style.display='none';
+    document.getElementById("desayunos").style.display='none';
+    document.getElementById("aperitivos").style.display='none';
+    document.getElementById("postres").style.display='flex';
+    document.getElementById("bebidas").style.display='none';
+    document.getElementById("clientes").style.display='none';
+    document.getElementById("pedidos").style.display='none';
+    document.getElementById("info").style.display='none';
+}
+function mostrarBebidas(){
+    document.getElementById("almuerzos").style.display='none';
+    document.getElementById("desayunos").style.display='none';
+    document.getElementById("aperitivos").style.display='none';
+    document.getElementById("postres").style.display='none';
+    document.getElementById("bebidas").style.display='flex';
+    document.getElementById("clientes").style.display='none';
+    document.getElementById("pedidos").style.display='none';
+    document.getElementById("info").style.display='none';
+}
+function mostrarClientes(){
+    document.getElementById("almuerzos").style.display='none';
+    document.getElementById("desayunos").style.display='none';
+    document.getElementById("aperitivos").style.display='none';
+    document.getElementById("postres").style.display='none';
+    document.getElementById("bebidas").style.display='none';
+    document.getElementById("clientes").style.display='flex';
+    document.getElementById("pedidos").style.display='none';
+    document.getElementById("info").style.display='none';
+}
+function mostrarPedidos(){
+    document.getElementById("almuerzos").style.display='none';
+    document.getElementById("desayunos").style.display='none';
+    document.getElementById("aperitivos").style.display='none';
+    document.getElementById("postres").style.display='none';
+    document.getElementById("bebidas").style.display='none';
+    document.getElementById("clientes").style.display='none';
+    document.getElementById("pedidos").style.display='flex';
+    document.getElementById("info").style.display='none';
+}
+function mostrarInfo(){
+    document.getElementById("almuerzos").style.display='none';
+    document.getElementById("desayunos").style.display='none';
+    document.getElementById("aperitivos").style.display='none';
+    document.getElementById("postres").style.display='none';
+    document.getElementById("bebidas").style.display='none';
+    document.getElementById("clientes").style.display='none';
+    document.getElementById("pedidos").style.display='none';
+    document.getElementById("info").style.display='flex';
+}
