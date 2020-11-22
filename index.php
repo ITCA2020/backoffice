58475442861bfec9d1f01e2a1545a86979b72358
Manuel Alberto Hernandez Melendez <65093082+MANUELh28@users.noreply.github.com>
2020-11-21 22:40:49 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+
+$usuario = "root";
+$contraseña = "";
+$servidor = "localhost";
+$basededatos="rinconchita";
+$conexion = mysqli_connect($servidor,$usuario,""); 
+$db = mysqli_select_db($conexion,$basededatos);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -70,22 +81,37 @@
                 
             </div>
             
-            <form action="">
+            <form action="" method="POST" enctype="multipart/form-data">
                 <div class="form lead">
                     <input type="text" placeholder="ID" class="form-control" readonly>
-                    <input type="text" placeholder="Nombre del almuerzo" class="form-control">
-                    <input type="number" placeholder="Precio" class="form-control">
-                    <input type="text" placeholder="Descripción" class="form-control">
+                    <input type="text" placeholder="Nombre del almuerzo" name="nombre_almuerzo" class="form-control">
+                    <input type="number" placeholder="Precio" name="precio_almuerzo" class="form-control">
+                    <input type="text" placeholder="Descripción" name="descripcion_almuerzo" class="form-control">
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es" required name="img_almuerzo">
                         <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
                     </div><hr>
-                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    <input type="submit" class="btn btn-primary" value="Guardar" name="guardar_almuerzo">
                     
                 </div>
             </form>
         </div>
         
+        <?php
+
+        if (isset($_POST["guardar_almuerzo"])) {
+ 
+   
+        $nombre_almuerzo = $_POST["nombre_almuerzo"];
+        $precio_almuerzo = $_POST["precio_almuerzo"];
+        $descripcion_almuerzo= $_POST["descripcion_almuerzo"];
+        $img_almuerzo = addslashes(file_get_contents($_FILES["img_almuerzo"]["tmp_name"]));
+
+        $registrar_almuerzo =  "INSERT INTO almuerzos (alm_nom,alm_precio,alm_desc,alm_img) VALUES ('$nombre_almuerzo', '$precio_almuerzo', '$descripcion_almuerzo', '$img_almuerzo')";
+        $regist_almuerzo  = mysqli_query($conexion,$registrar_almuerzo);
+ }
+
+        ?>
         
         
         <!--
@@ -99,23 +125,38 @@
             
             <div class="" id="msj_desayuno"></div>
             
-            <form action="">
+            <form action="" method="POST" enctype="multipart/form-data">
                 <div class="form lead">
                     <input type="text" placeholder="ID" class="form-control" readonly>
-                    <input type="text" placeholder="Nombre del desayuno" class="form-control">
-                    <input type="number" placeholder="Precio" class="form-control">
-                    <input type="text" placeholder="Descripción" class="form-control">
+                    <input type="text" placeholder="Nombre del desayuno" class="form-control" name="nombre_desayuno">
+                    <input type="number" placeholder="Precio" class="form-control" name="precio_desayuno">
+                    <input type="text" placeholder="Descripción" class="form-control" name="descripcion_desayuno">
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es" required name="img_desayuno">
                         <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
                     </div><hr>
-                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    <input type="submit" class="btn btn-primary" value="Guardar" name="guardar_desayuno">
                     
                 </div>
             </form>
         </div>
         
-        
+         <?php
+
+        if (isset($_POST["guardar_desayuno"])) {
+ 
+   
+        $nombre_desayuno = $_POST["nombre_desayuno"];
+        $precio_desayuno = $_POST["precio_desayuno"];
+        $descripcion_desayuno = $_POST["descripcion_desayuno"];
+        $img_desayuno = addslashes(file_get_contents($_FILES["img_desayuno"]["tmp_name"]));
+
+        $registrar_desayuno =  "INSERT INTO desayunos (des_nom,des_precio,des_desc,des_img) VALUES ('$nombre_desayuno', '$precio_desayuno', '$descripcion_desayuno', '$img_desayuno')";
+        $regist_desayuno = mysqli_query($conexion,$registrar_desayuno);
+ }
+
+
+        ?>
         
         <!--
         *    *    *    *    *    *    *    *    *    *    *    *
@@ -127,22 +168,37 @@
             <h1>Registrar Aperitivos</h1><hr>
             <div class="" id="msj_aperitivo"></div>
             
-            <form action="">
+            <form action="" method="POST" enctype="multipart/form-data">
                 <div class="form lead">
                     <input type="text" placeholder="ID" class="form-control" readonly>
-                    <input type="text" placeholder="Nombre del aperitivo" class="form-control">
-                    <input type="number" placeholder="Precio" class="form-control">
-                    <input type="text" placeholder="Descripción" class="form-control">
+                    <input type="text" placeholder="Nombre del aperitivo" class="form-control" name="nombre_aperitivo">
+                    <input type="number" placeholder="Precio" class="form-control" name="precio_aperitivo">
+                    <input type="text" placeholder="Descripción" class="form-control" name="descripcion_aperitivo">
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es" required name="img_aperitivo">
                         <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
                     </div><hr>
-                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    <input type="submit" class="btn btn-primary" value="Guardar" name="guardar_aperitivo">
                     
                 </div>
             </form>
         </div>
         
+        <?php
+
+        if (isset($_POST["guardar_aperitivo"])) {
+ 
+   
+        $nombre_aperitivo = $_POST["nombre_aperitivo"];
+        $precio_aperitivo = $_POST["precio_aperitivo"];
+        $descripcion_aperitivo = $_POST["descripcion_aperitivo"];
+        $img_aperitivo = addslashes(file_get_contents($_FILES["img_aperitivo"]["tmp_name"]));
+
+        $registrar_aperitivo =  "INSERT INTO aperitivos (ape_nom,ape_precio,ape_desc,ape_img) VALUES ('$nombre_aperitivo', '$precio_aperitivo', '$descripcion_aperitivo', '$img_aperitivo')";
+        $regist_aperitivo = mysqli_query($conexion,$registrar_aperitivo);
+ }
+
+        ?>
           
         
         <!--
@@ -155,23 +211,37 @@
             <h1>Registrar Postres</h1><hr>
             <div class="" id="msj_postre"></div>
             
-            <form action="">
+            <form action="" method="POST" enctype="multipart/form-data">
                 <div class="form lead">
                     <input type="text" placeholder="ID" class="form-control" readonly>
-                    <input type="text" placeholder="Nombre del postre" class="form-control">
-                    <input type="number" placeholder="Precio" class="form-control">
-                    <input type="text" placeholder="Descripción" class="form-control">
+                    <input type="text" placeholder="Nombre del postre" class="form-control"  name="nombre_postres">
+                    <input type="number" placeholder="Precio" class="form-control" name="precio_postres">
+                    <input type="text" placeholder="Descripción" class="form-control" name="descripcion_postres">
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es" required name="img_postres">
                         <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
                     </div><hr>
-                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    <input type="submit" class="btn btn-primary" value="Guardar" name="guardar_postres">
                     
                 </div>
             </form>
         </div>
         
-        
+          <?php
+
+        if (isset($_POST["guardar_postres"])) {
+ 
+   
+        $nombre_postres = $_POST["nombre_postres"];
+        $precio_postres = $_POST["precio_postres"];
+        $descripcion_postres = $_POST["descripcion_postres"];
+        $img_postres = addslashes(file_get_contents($_FILES["img_postres"]["tmp_name"]));
+        $registrar_postres =  "INSERT INTO postres (pos_nom,pos_precio,pos_desc,pos_img) VALUES ('$nombre_postres', '$precio_postres', '$descripcion_postres', '$img_postres')";
+        $regist_postres= mysqli_query($conexion,$registrar_postres);
+ }
+
+        ?>
+          
         
         <!--
         *    *    *    *    *    *    *    *    *    *    *    *
@@ -183,22 +253,37 @@
             <h1>Registrar Bebidas</h1><hr>
             <div class="" id="msj_bebida"></div>
             
-            <form action="">
+            <form action="" method="POST" enctype="multipart/form-data">
                 <div class="form lead">
                     <input type="text" placeholder="ID" class="form-control" readonly>
-                    <input type="text" placeholder="Nombre de bebida" class="form-control">
-                    <input type="number" placeholder="Precio" class="form-control">
-                    <input type="text" placeholder="Descripción" class="form-control">
+                    <input type="text" placeholder="Nombre de bebida" class="form-control" name="nombre_bebidas">
+                    <input type="number" placeholder="Precio" class="form-control" name="precio_bebidas">
+                    <input type="text" placeholder="Descripción" class="form-control" name="descricion_bebidas">
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFileLang" lang="es">
+                        <input type="file" class="custom-file-input" id="customFileLang" lang="es" required name="img_bebidas">
                         <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label> 
                     </div><hr>
-                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    <input type="submit" class="btn btn-primary" value="Guardar" name="guardar_bebidas">
                     
                 </div>
             </form>
         </div>
         
+          <?php
+
+        if (isset($_POST["guardar_bebidas"])) {
+ 
+   
+        $nombre_bebidas = $_POST["nombre_bebidas"];
+        $precio_bebidas = $_POST["precio_bebidas"];
+        $descripcion_bebidas = $_POST["descripcion_bebidas"];
+         $img_bebidas = addslashes(file_get_contents($_FILES["img_bebidas"]["tmp_name"]));
+        $registrar_bebidas =  "INSERT INTO bebidas (beb_nom,beb_precio,beb_desc,beb_img) VALUES ('$nombre_bebidas', '$precio_bebidas', '$descripcion_bebidas', '$img_bebidas')";
+        $regist_bebidas = mysqli_query($conexion,$registrar_bebidas);
+ }
+
+        ?>
+          
         
         <!--
         *    *    *    *    *    *    *    *    *    *    *    *
@@ -210,16 +295,29 @@
             <h1>Información de Contacto</h1><hr>
             <div class="" id="msj_bebida"></div>
             
-            <form action="">
+            <form action="index.php" method="post">
                 <div class="form lead">
-                    <input type="text" placeholder="Número de Whatsapp" class="form-control">
-                    <input type="number" placeholder="Número de Teléfono" class="form-control">
+                    <input type="text" placeholder="Número de Whatsapp" class="form-control" name="wasap_info">
+                    <input type="number" placeholder="Número de Teléfono" class="form-control" name="numero_info">
                     <hr>
-                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    <input type="submit" class="btn btn-primary" value="Guardar" name="guardar_info">
                     
                 </div>
             </form>
         </div>
+
+          <?php
+
+        if (isset($_POST["guardar_info"])) {
+ 
+   
+        $wasap_info = $_POST["wasap_info"];
+        $numero_info = $_POST["numero_info"];
+        $registrar_info =  "INSERT INTO info (wsp,tel) VALUES ('$wasap_info', '$numero_info')";
+        $regist_info = mysqli_query($conexion,$registrar_info);
+ }
+
+        ?>
 
     </main><!-- /.container -->
 
